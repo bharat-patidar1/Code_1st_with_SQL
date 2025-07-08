61196f6f71ec8d748ec70476190a8a25423f84e7
Bharat Patidar <bharatpatidar399@gmail.com>
2025-07-08 06:10:13 +0530

    hosted

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://code-1st-healthcare.onrender.com",
     credentials: true
 }));
 
