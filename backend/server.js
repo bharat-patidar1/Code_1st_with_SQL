430645f8f93048bfc4a67b5111e68f2be1a8d144
Bharat Patidar <bharatpatidar399@gmail.com>
2025-07-09 02:02:25 +0530

    before sql

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "https://code-1st-healthcare.onrender.com",
+    origin: "http://localhost:5173",
     credentials: true
 }));
 
